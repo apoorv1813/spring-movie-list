0d6d1b71753f71da08d635d6ef90bed35efbed8f
Apoorv Jain <apoorv1813@gmail.com>
2022-09-18 20:02:51 +0530

    movie-list | Eureka discovery-service | making catalog, info & ratings Eureka clients

---
+++ discovery-server/src/test/java/com/apoorv1813/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apoorv1813.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
